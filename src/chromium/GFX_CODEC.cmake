6b1ee90ce71765bd3a604e093b95ef0015f968ba
derofim <derofim@yandex.ru>
2019-05-22 02:05:04 +0300

    fixed GFX_CODEC

--- src/chromium/GFX_CODEC.cmake
+++ src/chromium/GFX_CODEC.cmake
@@ -1,11 +1,12 @@
-### --- GFX_CODEC ---###
+﻿### --- GFX_CODEC ---###
 
 set(GFX_CODEC_SOURCES
   #${GFX_CODEC_DIR}codec_export.h
   # TODO # ${GFX_CODEC_DIR}jpeg_codec.cc
+  ${GFX_CODEC_DIR}jpeg_codec.cc
   ${GFX_CODEC_DIR}jpeg_codec.h
   # TODO # ${GFX_CODEC_DIR}png_codec.cc
-  ${GFX_CODEC_DIR}png_codec.h
+  ${GFX_CODEC_DIR}png_codec.cc
   ${GFX_CODEC_DIR}png_codec.h
   ${GFX_CODEC_DIR}vector_wstream.cc
   ${GFX_CODEC_DIR}vector_wstream.h
@@ -19,7 +20,14 @@ add_library(GFX_CODEC STATIC
   ${GFX_CODEC_SOURCES}
 )
 
+if(${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
+  set(libjpeg_LIB libjpeg)
+  set(libpng_LIB libpng)
+endif()
+
 target_link_libraries(GFX_CODEC PRIVATE
+  ${libjpeg_LIB}
+  ${libpng_LIB}
   dynamic_annotations
   ${BASE_LIBRARIES}
   GFX_GEOMETRY_SKIA
