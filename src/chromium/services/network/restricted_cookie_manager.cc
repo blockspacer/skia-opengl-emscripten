ccd2d66286402d3d9c3880af0315c6a7e15cf659
derofim <derofim@yandex.ru>
2019-06-07 00:03:27 +0300

    fix typo in restricted_cookie_manager.cc

--- src/chromium/services/network/restricted_cookie_manager.cc
+++ src/chromium/services/network/restricted_cookie_manager.cc
@@ -1,10 +1,10 @@
-// Copyright 2017 The Chromium Authors. All rights reserved.
+﻿// Copyright 2017 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "services/network/restricted_cookie_manager.h"
 
-#include <memory>/home/avakimov_am/skia-opengl-emscripten/src/chromium/net/http/http_auth_handler_factory.cc:60:28:
+#include <memory>
 #include <utility>
 #include <vector>
 
