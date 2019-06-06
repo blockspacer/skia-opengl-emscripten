10cb89daf1b9ee5fa1ad606cf6f424762eebff8b
trofimov_d_a <trofimov_d_a@magnit.ru>
2019-06-06 16:24:01 +0300

    build with -j6

--- tools/emscriptenBuildVars.cmake
+++ tools/emscriptenBuildVars.cmake
@@ -3,7 +3,7 @@
 # NOTE: used ONLY by EMSCRIPTEN toolchain
 set(EXTRA_EMCMAKE_OPTS  "" CACHE STRING "emcmake arguments")
 
-set(EXTRA_EMMAKE_OPTS  "-j" "8" CACHE STRING "emmake arguments")
+set(EXTRA_EMMAKE_OPTS  "-j" "6" CACHE STRING "emmake arguments")
 
 find_program_required(EMSCRIPTEN_CMAKE "emcmake" "emcmake not found, search for EMSCRIPTEN`s emsdk_env.sh")
 
--- tools/unixBuildVars.cmake
+++ tools/unixBuildVars.cmake
@@ -3,6 +3,6 @@
 # NOTE: used ONLY by EMSCRIPTEN toolchain
 set(EXTRA_CMAKE_OPTS  "" CACHE STRING "emcmake arguments")
 
-set(EXTRA_MAKE_OPTS  "-j" "8" CACHE STRING "emmake arguments")
+set(EXTRA_MAKE_OPTS  "-j" "6" CACHE STRING "emmake arguments")
 
 find_program_required(MAKE_COMMAND "make" "make not found, search for clang-6 & gcc")
