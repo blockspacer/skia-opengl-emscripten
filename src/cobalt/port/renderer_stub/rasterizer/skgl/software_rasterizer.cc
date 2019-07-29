1c2c0a8494273f5775817c95bc7ff15781777c67
Unknown <unknown@example.com>
2019-07-29 08:02:24 +0300

    FIX: surface "blinks" without mutex (USE_PIMG_MUTEX 1)

--- src/cobalt/port/renderer_stub/rasterizer/skgl/software_rasterizer.cc
+++ src/cobalt/port/renderer_stub/rasterizer/skgl/software_rasterizer.cc
@@ -60,7 +60,8 @@ static sk_sp<SkTypeface> sktp;
 static const float FONT_SIZE_F = 22.0f;
 
 // TODO: replace mutex with task queue/emscripten_async_run_in_main_runtime_thread
-//#define USE_PIMG_MUTEX 1
+// NOTE: surface "blinks" without mutex
+#define USE_PIMG_MUTEX 1
 
 namespace cobalt {
 namespace renderer {
