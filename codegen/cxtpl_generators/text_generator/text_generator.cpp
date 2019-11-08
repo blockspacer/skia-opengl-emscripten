5f2d3ae6b4fe30a55885d04d045ffd7a7e70b2be
trofimov_d_a <trofimov_d_a@magnit.ru>
2019-11-08 12:44:01 +0300

    updated CXTPL generator

--- codegen/cxtpl_generators/text_generator/text_generator.cpp
+++ codegen/cxtpl_generators/text_generator/text_generator.cpp
@@ -1,8 +1,16 @@
 // --- SETUP ---
 
+/// \note define for easier debugging of that source file
+// #define _ENABLE_DEBUG_PRINTF 1
+
+/// \note include here all headers that may be used by `.cxtpl` templates
 #include <string>
 #include <cstdio>
 #include <fstream>
+#include <vector>
+#include <map>
+#include <sstream>
+#include <algorithm>
 
 // __has_include is currently supported by GCC and Clang. However GCC 4.9 may have issues and
 // returns 1 for 'defined( __has_include )', while '__has_include' is actually not supported:
@@ -21,25 +29,52 @@ namespace fs = std::experimental::filesystem;
 
 // template params
 #if defined(ENABLE_NATIVE_HTML)
-static std::string g_local_file_prefix = "";
+#define TO_STR2(x) #x
+#define TO_STR(x) TO_STR2(x)
+/// \note WEBSITE_ROOT_URL must be string in quotes
+static std::string g_local_file_prefix = WEBSITE_ROOT_URL;
 #else
 static std::string g_local_file_prefix = "file:///";
-#endif
+#endif // ENABLE_NATIVE_HTML
 
-static std::string LOCAL_FILE(const std::string& filepath) {
+static std::string g_platform_name = PLATFORM_NAME;
+
+enum class PlatformType {
+  WIN,
+  MAC,
+  ANDROID,
+  LINUX,
+  WEBGL,
+  HTML5
+};
+
+static PlatformType g_platform_type = PLATFORM_TYPE;
+
+static std::string local_path_prefix(const std::string& filepath) {
+#if defined(_ENABLE_DEBUG_PRINTF)
+  printf("LOCAL_FILE prefix %s\n", g_local_file_prefix.c_str());
+#endif // _ENABLE_DEBUG_PRINTF
   return g_local_file_prefix + filepath;
 }
 
+#define LOCAL_FILE local_path_prefix
+
 /// \note cxtpl_output will be changed from another file (from Cling)
+/// \todo remove that global variable: get `cxtpl_output` as cling::Value and pass it into onAfterTemplateGeneration
 std::string cxtpl_output;
 
 void writeToFile(const std::string& str, const std::string& file_path) {
+/*#if defined(_ENABLE_DEBUG_PRINTF)
+  printf("writeToFile %s %s\n", file_path.c_str(), str.substr(0, 100).c_str());
+#endif // _ENABLE_DEBUG_PRINTF*/
+
     fs::create_directories(fs::path(file_path).parent_path());
 
     std::ofstream ofs(file_path);
     if(!ofs) {
         // TODO: better error reporting
         printf("ERROR: can`t write to file %s\n", file_path.c_str());
+        /// \todo better error reporting
         return;
     }
     ofs << str;
@@ -47,6 +82,7 @@ void writeToFile(const std::string& str, const std::string& file_path) {
     if(!ofs)    //bad() function will check for badbit
     {
         printf("ERROR: writing to file failed %s\n", file_path.c_str());
+        /// \todo better error reporting
         return;
     }
 }
